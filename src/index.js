b58e671ad9ecbbf8ea442378e1d2b9c51e9ac496
Divya Chauhan <dvchhn7@gmail.com>
2020-05-21 13:39:26 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as Survey from 'survey-react';
+
+import "../css/sideBar.css";
+import "../css/topBar.css";
+import "../css/home.css";
+import "../css/modern.css";
+import "survey-react/survey.css";
+
+import HomePage from './home.jsx';
+
+ReactDOM.render(<HomePage />, document.getElementById("root"));
