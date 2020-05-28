b435a6accb0ad13655850869f00119b1d462c6d6
Divya Chauhan <dvchhn7@gmail.com>
2020-05-28 21:24:21 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as Survey from 'survey-react';
 
-// import "../css/sideBar.css";
-// import "../css/topBar.css";
-// import "../css/home.css";
-// import "survey-react/survey.css"; // This is overriding the Page Title, so removed for now
 import "../css/modern.css";
 import "../css/style.css";
 
